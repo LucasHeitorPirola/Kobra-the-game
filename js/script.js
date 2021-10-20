1217372d9d5441bcc158acdc76d3a94e271e9cb3
LucasHeitorPirola <lucas_heitor_pirola@hotmail.com>
2021-10-19 23:46:30 -0300

    Desenvolvimento do jogo terminado, mas ainda haverão alterações para melhorá-lo!

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function criarBG() {
 }
 
 function criarCobrinha() {
-  for(i=0; i < snake.length; i++){
+  for(i = 0; i < snake.length; i++){
     context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
@@ -29,26 +29,37 @@ function drawFood(){
   context.fillRect(food.x, food.y, box, box);
 }
 
-document.addEventListener("keydown", update);
+document.addEventListener('keydown', update);
 
-function update(event) {
-  if(event.keycode == 37 && direction != "right") direction = "left";
-  if(event.keycode == 38 && direction != "down") direction = "up";
-  if(event.keycode == 39 && direction != "left") direction = "right";
-  if(event.keycode == 40 && direction != "up") direction = "down";
+function update(event){
+  if(event.keyCode == 37 && direction != 'right') direction = 'left';
+  if(event.keyCode == 38 && direction != 'down') direction = 'up';
+  if(event.keyCode == 39 && direction != 'left') direction = 'right';
+  if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
 function iniciarJogo() {
  
-  if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-  if(snake[0].x > 0 && direction == "left") snake[0].x = 16 * box;
-  if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-  if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+  if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0
+  if(snake[0].x > 15 * box && direction == 'up') snake[0].x = 0
+  if(snake[0].x > 15 * box && direction == 'down') snake[0].x = 0
+
+  if(snake[0].x < 0  && direction == 'left') snake[0].x = 15 * box
+  if(snake[0].x < 0  && direction == 'up') snake[0].x = 15 * box
+  if(snake[0].x < 0  && direction == 'down') snake[0].x = 15 * box
+
+  if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0
+  if(snake[0].y > 15 * box && direction == 'right') snake[0].y = 0
+  if(snake[0].y > 15 * box && direction == 'left') snake[0].y = 0
+
+  if(snake[0].y < 0 && direction == 'up') snake[0].y = 15 * box
+  if(snake[0].y < 0 && direction == 'right') snake[0].y = 15 * box
+  if(snake[0].y < 0 && direction == 'left') snake[0].y = 15 * box
 
   for(i = 1; i < snake.length; i++){
     if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
       clearInterval(jogo);
-      alert("Game Over :( ");
+      alert("Game Over :(");
     }
   }
 
